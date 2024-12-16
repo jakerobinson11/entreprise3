7dce80851ed90cc20ad94a07148997c5d701dc08
jakerobinson11 <jake.robinson11@gmail.com>
2024-12-16 16:20:33 +0100

    added form to add

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,9 +17,26 @@ class EmployeController extends AbstractController
     {
         $repository = $doctrine->getRepository(Employe::class);
         $employes = $repository->findAll();
-        dd($employes);
-        return $this->render('employe/index.html.twig',[
-            "mpls"=>$employes
+        /* dd($employes); */
+        return $this->render('employe/index.html.twig', [
+            "mpls" => $employes
+        ]);
+    }
+
+    #[Route('/employe/add', name: 'add_employe')]
+    public function add(ManagerRegistry $doctrine, Request $request)
+    {
+        $employe = new Employe();
+        $form = $this->createForm(EmployeType::class, $employe);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $doctrine->getManager();
+            $manager->persist($employe);
+            $manager->flush();
+            $this->redirectToRoute('add_employe');
+        }
+        return $this->render('employe/add.html.twig', [
+            "formulaire" => $form->createView()
         ]);
     }
 }
